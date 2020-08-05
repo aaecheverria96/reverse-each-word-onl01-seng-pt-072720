5204ec8d472cce4cc332d78649723e99fe67af03
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-05 20:15:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word
-  reverse_each_word.reverse("Hello there, and how are you?")
-return reverse_each_word
+def reverse_each_word (sentence)
+  sentence.reverse("Hello there, and how are you?")
+return sentence 
 end
